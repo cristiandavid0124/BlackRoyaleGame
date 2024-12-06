721527c8a4b34b9ae114d764959a7eae9b00b1e2
cristiandavid0124 <cristian.naranjo@mail.escuelaing.edu.co>
2024-12-06 18:37:03 -0500

    corrigiendo conexion base de datos despliegue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+# Verificar si MONGO_URI está configurado
+if [ -z "$MONGO_URI" ]; then
+  echo "ERROR: La variable de entorno MONGO_URI no está configurada."
+  exit 1
+fi
+
 # Iniciar la aplicación Spring Boot en segundo plano
+echo "Iniciando la aplicación Spring Boot..."
 java -jar /app/app.jar --server.address=0.0.0.0 &
 
 # Iniciar Nginx en primer plano
+echo "Iniciando Nginx..."
 nginx -g 'daemon off;'
