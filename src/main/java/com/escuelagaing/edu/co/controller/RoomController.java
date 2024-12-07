5821922939c57a04bf2a834b6902798de22c6993
cristiandavid0124 <cristian.naranjo@mail.escuelaing.edu.co>
2024-12-06 19:08:05 -0500

    agregando documentacion

--- src/main/java/com/escuelagaing/edu/co/controller/RoomController.java
+++ src/main/java/com/escuelagaing/edu/co/controller/RoomController.java
@@ -1,17 +1,24 @@
 package com.escuelagaing.edu.co.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import com.escuelagaing.edu.co.dto.RoomDTO;
 import com.escuelagaing.edu.co.model.Room;
 import com.escuelagaing.edu.co.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "https://blackjackroyaleapp-f6hagcdvc5bbejb0.canadacentral-01.azurewebsites.net")
 @RequestMapping("/api/rooms")
+@Tag(name = "Room Controller", description = "Endpoints para la gestión de salas. Permite crear, actualizar, obtener y eliminar salas en el sistema educativo.")
 public class RoomController {
 
     private final RoomService roomService;
@@ -20,11 +27,19 @@ public class RoomController {
     public RoomController(RoomService roomService) {
         this.roomService = roomService;
     }
-
+    @Operation(summary = "Obtener todas las salas", description = "Este endpoint devuelve una lista de todas las salas disponibles en el sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de salas obtenida con éxito")
+    })
     @GetMapping
     public List<Room> getAllRooms() {
         return roomService.getAllRooms();
     }
+    @Operation(summary = "Obtener una sala por ID", description = "Este endpoint devuelve los detalles de una sala específica basada en su ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sala encontrada"),
+            @ApiResponse(responseCode = "404", description = "Sala no encontrada")
+    })
 
     @GetMapping("/{roomId}")
     public ResponseEntity<Room> getRoomById(@PathVariable String roomId) {
@@ -33,17 +48,32 @@ public class RoomController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+
+
+    @Operation(summary = "Crear una sala", description = "Este endpoint crea una nueva sala con el ID especificado. Devuelve los detalles de la sala creada.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Sala creada con éxito")
+    })
     @PostMapping("/{roomId}")
     public Room createRoom(@PathVariable String roomId) {
         return roomService.createRoom(roomId);
     }
 
+
+    @Operation(summary = "Actualizar una sala", description = "Este endpoint permite actualizar los detalles de una sala existente.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sala actualizada con éxito")
+    })
     @PutMapping
     public Room updateRoom(@RequestBody RoomDTO roomDTO) {
         return roomService.updateRoom(roomDTO);
     }
 
-
+    @Operation(summary = "Eliminar una sala", description = "Este endpoint elimina una sala específica por su ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Sala eliminada con éxito"),
+            @ApiResponse(responseCode = "404", description = "Sala no encontrada")
+    })
     @DeleteMapping("/{roomId}")
     public ResponseEntity<Void> deleteRoom(@PathVariable String roomId) {
         roomService.deleteRoom(roomId);
--- src/main/java/com/escuelagaing/edu/co/controller/UserController.java
+++ src/main/java/com/escuelagaing/edu/co/controller/UserController.java
@@ -8,6 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +23,7 @@ import org.slf4j.LoggerFactory;
 @RestController
 @CrossOrigin(origins = "https://blackjackroyaleapp-f6hagcdvc5bbejb0.canadacentral-01.azurewebsites.net")
 @RequestMapping("/api/users")
+@Tag(name = "User Controller", description = "Endpoints para gestionar usuarios en el sistema, incluyendo creación, actualización, eliminación y consulta.")
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
@@ -27,7 +33,11 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
+    @Operation(summary = "Crear un nuevo usuario", description = "Este endpoint permite crear un nuevo usuario en el sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario creado con éxito"),
+            @ApiResponse(responseCode = "400", description = "Solicitud incorrecta")
+    })
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody UserDTO userDto) {
         try {
@@ -51,7 +61,11 @@ public class UserController {
         }
     }
 
-
+    @Operation(summary = "Obtener el historial de juegos de un usuario", description = "Este endpoint devuelve el historial de juegos de un usuario específico por su ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Historial de juegos obtenido con éxito"),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado")
+    })
     public ResponseEntity<List<Map<String, Object>>> getUserGameHistory(@PathVariable String id) {
         try {
             List<Map<String, Object>> gameHistory = userService.getUserGameHistory(id);
@@ -61,7 +75,11 @@ public class UserController {
         }
     }
 
-    // Obtener un usuario por ID
+    @Operation(summary = "Obtener un usuario por ID", description = "Este endpoint devuelve los detalles de un usuario por su ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario encontrado"),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable String id) {
         Optional<User> user = userService.getUserById(id);
@@ -69,16 +87,16 @@ public class UserController {
                    .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    // Actualizar un usuario
+    @Operation(summary = "Actualizar un usuario", description = "Este endpoint permite actualizar los detalles de un usuario existente.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario actualizado con éxito"),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable String id, @RequestBody UserDTO userDetails) {
         try {
-            // Convertir el DTO a la entidad
             User user = new User();
-            
             user.setNickName(userDetails.getNickName());
-            // Mapear otros campos según sea necesario
-            // Llamar al servicio de actualización
             User updatedUser = userService.updateUser(id, user);
             return ResponseEntity.ok(updatedUser);
         } catch (RuntimeException e) {
@@ -87,7 +105,11 @@ public class UserController {
     }
     
 
-    // Eliminar un usuario
+    @Operation(summary = "Eliminar un usuario", description = "Este endpoint elimina un usuario del sistema por su ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Usuario eliminado con éxito"),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable String id) {
         try {
